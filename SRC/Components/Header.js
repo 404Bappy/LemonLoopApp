1b0da7657fe967d233900228dde2e2db779abe3a
Bappy71 <19103076@iubat.edu>
2023-08-22 15:39:37 +0600

    first commit

--- SRC/Components/Header.js
+++ SRC/Components/Header.js
@@ -4,8 +4,15 @@ import React from 'react';
 export default function Header() {
   return (
     <View style={{flex: 0.2, backgroundColor: '#F4CE14'}}>
-      <Text style={{padding: 25, fontSize: 30, color: 'black'}}>
-        Welcome To <Text style={{}}>LemonLoop</Text>
+      <Text
+        style={{
+          padding: 25,
+          fontSize: 40,
+          color: 'black',
+          textAlign: 'center',
+        }}
+        numberOfLines={2}>
+        Welcome To <Text style={{fontWeight: 'bold'}}>Lemon Loop</Text>
       </Text>
     </View>
   );
